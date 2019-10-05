105888a14cd2034d9811b52e90190d4da00e0849
wangyi <394489664@qq.com>
2019-10-05 11:52:11 +0800

    (*)功能： 0、项目初始化话 1、oauth2 授权码方式 且基于内存

---
+++ spring-security-oauth2-server/src/main/java/com/example/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * TODO
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter{
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret(passwordEncoder.encode("secret"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("app")
+                .redirectUris("http://www.baidu.com");
+    }
+}
