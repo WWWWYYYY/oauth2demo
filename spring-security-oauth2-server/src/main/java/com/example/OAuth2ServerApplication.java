396d056f4798f7b4944f17d26e4e428d2ca1e8f4
wangyi <394489664@qq.com>
2019-10-06 18:43:51 +0800

    (*)功能： 0、增加对tk.mybatis支持

--- spring-security-oauth2-server/src/main/java/com/example/OAuth2ServerApplication.java
+++ spring-security-oauth2-server/src/main/java/com/example/OAuth2ServerApplication.java
@@ -2,7 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
+
+@EnableTransactionManagement
+@MapperScan(basePackages = "com.example.mapper")
 @SpringBootApplication
 public class OAuth2ServerApplication {
 
