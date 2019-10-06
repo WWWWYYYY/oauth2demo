0e0dd61817620bab3d1158ace88623cf4c742398
wangyi <394489664@qq.com>
2019-10-06 20:38:54 +0800

    (*)功能： 0、创建资源项目并配置相关配置

---
+++ spring-security-oauth2-resource/src/main/java/com/example/oauth2/App.java
@@ -0,0 +1,14 @@
+package com.example.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}  
