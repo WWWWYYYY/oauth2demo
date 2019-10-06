086468156ca4a898e3065600ca626da933878e4b
wangyi <394489664@qq.com>
2019-10-06 23:24:32 +0800

    (*)功能： 0、资源服务配置

--- spring-security-oauth2-resource/src/main/java/com/example/oauth2/App.java
+++ spring-security-oauth2-resource/src/main/java/com/example/oauth2/App.java
@@ -2,10 +2,12 @@ package com.example.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * TODO
  */
+@MapperScan(basePackages = "com.example.oauth2.mapper")
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
