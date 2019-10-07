cbc04fc507609b66eb491ff41a145a5d6f58ed31
wangyi <394489664@qq.com>
2019-10-07 21:57:25 +0800

    (*)功能： 0、资源服务添加oauth认证

--- spring-security-oauth2-resource/src/main/java/com/example/oauth2/App.java
+++ spring-security-oauth2-resource/src/main/java/com/example/oauth2/App.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
- * TODO
+ * 教程视频地址：https://www.bilibili.com/video/av48590637/?p=17
+ * 接入oauth2以后 资源将不可直接访问。需要获取到access_token 才能访问
+ * 1、访问 http://localhost:9999/oauth/authorize?client_id=client&response_type=code 返回 code
+ * 2、获取token
+ *  * curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'grant_type=authorization_code&code=1JuO6V' "http://client:secret@localhost:9999/oauth/token"
+ * 3、访问资源服务时携带token
  */
 @MapperScan(basePackages = "com.example.oauth2.mapper")
 @SpringBootApplication
--- spring-security-oauth2-resource/src/main/java/com/example/oauth2/config/ResourceServerConfiguration.java
+++ spring-security-oauth2-resource/src/main/java/com/example/oauth2/config/ResourceServerConfiguration.java
@@ -1,36 +1,46 @@
-//package com.example.oauth2.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//
-//@Configuration
-//@EnableResourceServer
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-//public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http
-//                .exceptionHandling()
-//                .and()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                // 以下为配置所需保护的资源路径及权限，需要与认证服务器配置的授权部分对应
-//                .antMatchers("/").hasAuthority("SystemContent")
-//                .antMatchers("/view/**").hasAuthority("SystemContentView")
-//                .antMatchers("/insert/**").hasAuthority("SystemContentInsert")
-//                .antMatchers("/update/**").hasAuthority("SystemContentUpdate")
-//                .antMatchers("/delete/**").hasAuthority("SystemContentDelete")
-//                .and()
-//                .formLogin().permitAll();
-//
-//        //session管理,失效后跳转
-//        http.sessionManagement().invalidSessionUrl("/login");
-//    }
-//
-//}
+package com.example.oauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+
+/**
+ *  http://localhost:9999/oauth/authorize?client_id=client&response_type=code
+ */
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    private static final String RESOURCE_ID = "oauth2-resource";
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        //resourceId 用于分配给可授予的clientId
+        resources.resourceId(RESOURCE_ID).stateless(true);
+    }
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .exceptionHandling()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // 以下为配置所需保护的资源路径及权限，需要与认证服务器配置的授权部分对应
+                .antMatchers("/").hasAuthority("SystemContent")
+                .antMatchers("/view/**").hasAuthority("SystemContentView")
+                .antMatchers("/insert/**").hasAuthority("SystemContentInsert")
+                .antMatchers("/update/**").hasAuthority("SystemContentUpdate")
+                .antMatchers("/delete/**").hasAuthority("SystemContentDelete");
+
+    }
+
+}
